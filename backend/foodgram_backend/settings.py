f5d61e2fe610c1484f0301e6730e485dde5b7b91
Efendiev Magomed <m.efendiev@avia-centr.ru>
2024-07-03 23:10:25 +0300

    Restart

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -76,8 +76,8 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "django"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", 5432),
-    },
+        "PORT": os.getenv("DB_PORT", 5432)
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
